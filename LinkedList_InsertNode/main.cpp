fba5b5b881a9d131230d7df00dbb1ac9b7e71c75
mitchcamza <192mitch88@gmail.com>
2022-09-10 15:11:32 +0200

    Initial Commit

---
+++ LinkedList_InsertNode/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  LinkedList_InsertNode
+//
+//  Created by Mitch Campbell on 2022/09/10.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
