3165a33d1a6b257fc5c8dff87d17b96705e5cf51
mitchcamza <192mitch88@gmail.com>
2022-09-17 16:59:37 +0200

    Implemented copy constructor and copyList method to make a copy of a linked list.

--- LinkedList/LinkedList.h
+++ LinkedList/LinkedList.h
@@ -9,24 +9,32 @@ template <class elemType>
 class LinkedListType
 {
 public:
-	LinkedListType();			// default constructor
-	~LinkedListType();			// destructor
-	void initializeList();		// initialize list as empty list
-	void destroyList();			// deallocate memory
-	void print() const;			// print the list
-	bool isEmpty() const;		// returns true if list is empty
-	int length() const;			// returns the number of nodes in the list
-	elemType front() const;		// returns the first node's info
-	elemType back() const;		// returns the last node's info
-	void insertFirst(const elemType& newItem);	// insert node at beginning
-	void insertLast(const elemType& newItem);	// insert node at the end
+	LinkedListType();								// default constructor
+	~LinkedListType();								// destructor
+	void initializeList();							// initialize list as empty list
+	void destroyList();								// deallocate memory
+	void print() const;								// print the list
+	bool isEmpty() const;							// returns true if list is empty
+	int length() const;								// returns the number of nodes in the list
+	elemType front() const;							// returns the first node's info
+	elemType back() const;							// returns the last node's info
+	void insertFirst(const elemType& newItem);		// insert node at beginning
+	void insertLast(const elemType& newItem);		// insert node at the end
 	void deleteNode(const elemType& deleteItem);	// delete a node from the list
 	void buildListForward();
 	void buildListBackward();
-	bool search(const elemType& searchItem);	// returns true if item is in the list
-	LinkedListIterator<elemType> begin();		// returns an iterator to the first node
-	LinkedListIterator<elemType> end();			// returns an iterator to the last node
+	bool search(const elemType& searchItem);		// returns true if item is in the list
+	LinkedListIterator<elemType> begin();			// returns an iterator to the first node
+	LinkedListIterator<elemType> end();				// returns an iterator to the last node
+	
+	void copyList(const LinkedListType<elemType>& otherList);	// make an identical copy of the linked list
 
+	LinkedListType (const LinkedListType<elemType>& otherList);	// copy constructor
+
+	// TODO: implement overloaded assignment operator
+	const LinkedListType<elemType>& operator= (const LinkedListType<elemType>& otherList);
+	
+	// TODO: implement insertAfter
 	
 	
 protected:
@@ -77,7 +85,7 @@ void LinkedListType<elemType>::print() const
 	NodeType<elemType> *current {first};
 	while (current != NULL)
 	{
-		std::cout << current->info << std::endl;
+		std::cout << current->info << " ";
 		current = current->link;
 	}
 }
@@ -275,6 +283,59 @@ LinkedListIterator<elemType> LinkedListType<elemType>::end()
 {
 	LinkedListIterator<elemType> temp {NULL};
 	return temp;
+}
+
+
+template <class elemType>
+LinkedListType<elemType>::LinkedListType(const LinkedListType<elemType>& otherList)
+{
+	first = NULL;
+	copyList(otherList);
+}
+
+
+template <class elemType>
+void LinkedListType<elemType>::copyList(const LinkedListType<elemType>& otherList)
+{
+	NodeType<elemType> *newNode = NULL;	// pointer to create a node
+	NodeType<elemType> *current = NULL;	// pointer to traverse the list
+	
+	// make sure the destination list is empty
+	if (first != NULL)
+	{
+		destroyList();
+	}
+	
+	if (otherList.first == NULL)	// if the source list is empty
+	{
+		first = NULL;
+		last = NULL;
+		count = 0;
+	}
+	
+	else
+	{
+		current = otherList.first;
+		count = otherList.count;
+		
+		// copy the first node
+		first = new NodeType<elemType>;
+		first->info = current->info;
+		first->link = NULL;
+		last = first;
+		current = current->link;
+		
+		// copy the remaining list
+		while (current != NULL)
+		{
+			newNode	= new NodeType<elemType>;
+			newNode->info = current->info;
+			newNode->link = NULL;
+			last->link = newNode;
+			last = newNode;
+			current = current->link;
+		}
+	}
 }
 
 #endif /* LinkedList_h */
--- LinkedList/main.cpp
+++ LinkedList/main.cpp
@@ -29,17 +29,26 @@ int main()
 	LinkedListType<int> myList;
 	
 	myList.buildListForward();
+	
+	std::cout << std::endl;
+	std::cout << "The original list: ";
 	myList.print();
 	
-	// search the list for a user-entered number
-	int number {};
-	std::cout << "Enter a number to search for: ";
-	std::cin >> number;
-	if (myList.search(number))
-		std::cout << number << " is in the list." << std::endl;
-	else
-		std::cout << number << " is not in the list." << std::endl;
+	// make a copy of myList using copy constructor
+	LinkedListType<int> yourList {myList};
+	std::cout << "\nSucessfully copied the list." << std::endl;
+	std::cout << "\nYour list: ";
+	yourList.print();
+	std::cout << std::endl;
 	
+	
+	// make a copy of the list using the copyList method
+	LinkedListType<int> listCopy;
+	listCopy.copyList(yourList);
+	std::cout << "\nSucessfully copied the list." << std::endl;
+	std::cout << "\nThe copied list: ";
+	listCopy.print();
+	std::cout << std::endl;
 	
 	return 0;
 }
